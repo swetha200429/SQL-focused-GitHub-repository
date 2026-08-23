7b183d723dc80c73aeef7d3a0583dee012f91372
swetha200429 <swetha200429@gmail.com>
2026-08-23 16:02:22 +0530

    Delete SQL Tutorial/Project/Warehouse-Inventory-Management directory

--- SQL Tutorial/Project/Warehouse-Inventory-Management/08_And.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM Inventory
-WHERE Category = 'Electronics'
-AND Warehouse_Location = 'Chennai';
-
-SELECT *
-FROM Inventory
-WHERE Quantity < 10
-AND Price > 1000;
--- SQL Tutorial/Project/Warehouse-Inventory-Management/13_Delete.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM Inventory
-WHERE Product_ID = 121;
-
-SELECT *
-FROM Inventory
-WHERE Product_ID = 121;
