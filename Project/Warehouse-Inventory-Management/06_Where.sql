d66e0845cf7593280d95e08de4e3ed88e730d06b
swetha200429 <swetha200429@gmail.com>
2026-08-23 16:02:59 +0530

    Add files via upload

---
+++ Project/Warehouse-Inventory-Management/06_Where.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM Inventory
+WHERE Category = 'Electronics';
+
+SELECT *
+FROM Inventory
+WHERE Warehouse_Location = 'Chennai';
+
+SELECT *
+FROM Inventory
+WHERE Quantity < 10;
+
+SELECT *
+FROM Inventory
+WHERE Price > 10000;
---
+++ Project/Warehouse-Inventory-Management/12_Update.sql
@@ -0,0 +1,7 @@
+UPDATE Inventory
+SET Quantity = 20
+WHERE Product_ID = 102;
+
+SELECT *
+FROM Inventory
+WHERE Product_ID = 102;
